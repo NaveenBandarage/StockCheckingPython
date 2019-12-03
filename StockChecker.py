36da47c0c38341c8e65e6dde4d137142ec10ee98
thebanterage <thebanterage@gmail.com>
2019-12-03 15:03:38 +1300

    Created Stock checker using yahoofin
    Need to explore more options with it.

---
+++ StockChecker.py
@@ -0,0 +1,7 @@
+#this import lets me gather live stock info. 
+from yahoo_fin import stock_info as stockInfo
+
+# get live price of Apple
+si.get_live_price("aapl")
+
+#There are more features but need to explore some more 
